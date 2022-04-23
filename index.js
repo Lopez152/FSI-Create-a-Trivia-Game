e31b193bea09cf50a690463f13bef97f8290f1a7
lex lopez <lexlopez2014@hotmail.com>
2022-04-22 22:30:49 -0700

    Changed font size on navbar

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-let userName = window.prompt('Lex:')
+let userName = window.prompt('Please enter your name:')
+for(let i= 0; i < questions.length; i++){
+    let question = questions[i]
+    let userAnswer = window.prompt(question.text)
+}
